264c429d4922a454d33a9e1f0d7655aab26497b6
emmanuelkiplangatbett <emmanuel.bett@student.moringa>
2023-02-09 15:17:27 +0300

    completed assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,19 @@
+require "pry"
 class Dog
+    #setter method
+    def name=(name)
+        @name=name
+    end
 
+    def breed=(breed)
+        @breed = breed
+    end
+#getter mettod
+    def name
+        @name
+    end
+
+    def breed
+        @breed
+    end
 end
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,24 @@
-class Person
 
-end
+require "pry"
+class Person
+    def name=(name)
+        @name = name
+      end
+
+      def job=(job)
+        @job=job
+      end
+
+     
+      # getter method
+       def name
+        @name
+       end
+
+       def job
+        @job
+       end
+
+ end
+
+
